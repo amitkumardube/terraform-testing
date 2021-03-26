b8de5b9a46226cadfe6aa66fde0d522b5af6da66
amitkumardube <amitkumardube@gmail.com>
2021-03-26 14:09:35 +0200

    added bucket

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "sa_email" {
     value = google_service_account.bqowner.email
 }
+
+output "bucket_name" {
+    value = google_storage_bucket.uat_bucket.url
+}
--- resource.tf
+++ resource.tf
@@ -26,6 +26,25 @@ resource "google_bigquery_dataset" "dataset" {
   }
 }
 
+resource "google_storage_bucket" "uat_bucket" {
+  name          = "amit_test_bucket_uat"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
+
 resource "google_service_account" "bqowner" {
   account_id = var.account_name
 }
